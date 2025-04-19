0f1f993d9bf56e026944114475c1677af1896f2a
Manasa Gowda <mgowda4388426@gmail.com>
2025-04-19 10:04:11 +0530

    first commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,7 +5,7 @@ export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
   return (
     <div className="Todo">
         <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-        <div>
+        <div className="options">
         <button className='edit' onClick={() => editTodo(task.id)}>Edit</button>
         <button className='delete' onClick={() => deleteTodo(task.id)}>Delete</button>
         </div>
